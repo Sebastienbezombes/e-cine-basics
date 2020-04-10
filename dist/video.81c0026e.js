93757e9a1fa8fa509bfaead579781b4d6e57b6b3
Alx0088 <alex.painnot@gmail.com>
2020-04-10 22:32:10 +0200

    Presque fini

--- dist/video.81c0026e.js
+++ dist/video.81c0026e.js
@@ -214,7 +214,12 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\assets\\flash.png":[["flash.9eaa7f28.png","assets/flash.png"],"assets/flash.png"],"_css_loader":"node_modules/parcel/src/builtins/css-loader.js"}],"node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\assets\\flash.png":[["flash.9eaa7f28.png","assets/flash.png"],"assets/flash.png"],"_css_loader":"node_modules/parcel/src/builtins/css-loader.js"}],"css/footer.css":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"_css_loader":"node_modules/parcel/src/builtins/css-loader.js"}],"node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -242,7 +247,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50189" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51762" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
